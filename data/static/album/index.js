b565125cdb69bca1c9837d5f1651b00f4b966396
Connor Slade <connor@connorcode.com>
2021-11-15 20:56:42 -0500

    🎆 Basicly Done

--- data/static/album/index.js
+++ data/static/album/index.js
@@ -1,20 +1,4 @@
-// const cover = document.querySelectorAll(".cover")[0];
-// const readme = document.querySelectorAll(".readme")[0];
-// let withHeight = false;
-//
-// new ResizeObserver(() => {
-//   console.log(`${readme.clientWidth}, ${cover.width}`);
-//   if (readme.clientWidth > cover.width && !withHeight) {
-//     cover.style.width = "100vw";
-//     cover.style.height = "";
-//     withHeight = true;
-//     return;
-//   }
-//
-//   cover.style.width = "";
-//   cover.style.height = "100%";
-//   withHeight = false;
-// }).observe(readme);
+let pop = document.querySelectorAll('.pop')[0];
 
 fetch(`${window.location.pathname}/photos`)
   .then((r) => r.json())
@@ -23,6 +7,21 @@ fetch(`${window.location.pathname}/photos`)
       let img = document.createElement("img");
       img.src = `${window.location.pathname}/photo/${item}`;
       img.classList.add("img");
+
+      img.addEventListener('click', () => {
+        pop.innerHTML = img.outerHTML;
+        pop.style.opacity = 1;
+        pop.style.pointerEvents = 'all';
+        document.body.style.overflow = 'hidden';
+      })
+
       document.querySelectorAll(".photos")[0].appendChild(img);
     })
   );
+
+pop.addEventListener('click', () => {
+  pop.style.opacity = 0;
+  pop.style.pointerEvents = 'none';
+  document.body.style.overflow = 'auto';
+  document.body.style.overflowX = 'hidden';
+});
